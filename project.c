1cdabb0102c6e00d4c4143a6f82a61fbf8d9c8a3
Karina G <31293496+kryptonite0523@users.noreply.github.com>
2017-12-06 08:09:31 -0500

    Editing

--- project.c
+++ project.c
@@ -25,18 +25,21 @@ __error__(char *pcFilename, uint32_t ui32Line)
 
 
 int main(void)
-{    unsigned long ulPeriod;
+{    
+	
+unsigned long Period;
+	
 
-    //Set the clock
+    //Set system clock
    SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC |   SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
 
-   //Configure PWM Clock to match system
+   //Configure PWM Clock
    SysCtlPWMClockSet(SYSCTL_PWMDIV_1);
 
-   // Enable the peripherals used by this program.
+   // Enable the peripherals 
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM1);  //The Tiva Launchpad has two modules (0 and 1). Module 1 covers the LED pins
-    ulPeriod = SysCtlClockGet() / 200; //PWM frequency 200HZ
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM1); 
+    Period = SysCtlClockGet() / 200;             //PWM frequency 200Hz
 
     //Configure PF1,PF2,PF3 Pins as PWM
     GPIOPinConfigure(GPIO_PF1_M1PWM5);
@@ -45,28 +48,27 @@ int main(void)
     GPIOPinTypePWM(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
 
     //Configure PWM Options
-    //PWM_GEN_2 Covers M1PWM4 and M1PWM5
-    //PWM_GEN_3 Covers M1PWM6 and M1PWM7
+    //PWM_GEN_2 --> M1PWM4 and M1PWM5
+    //PWM_GEN_3 --> M1PWM6 and M1PWM7
     PWMGenConfigure(PWM1_BASE, PWM_GEN_2, PWM_GEN_MODE_UP_DOWN | PWM_GEN_MODE_NO_SYNC); 
     PWMGenConfigure(PWM1_BASE, PWM_GEN_3, PWM_GEN_MODE_UP_DOWN | PWM_GEN_MODE_NO_SYNC); 
 
-    //Set the Period (expressed in clock ticks)
-    PWMGenPeriodSet(PWM1_BASE, PWM_GEN_2, ulPeriod);
-    PWMGenPeriodSet(PWM1_BASE, PWM_GEN_3, ulPeriod);
+    //Set the Period
+    PWMGenPeriodSet(PWM1_BASE, PWM_GEN_2, Period);
+    PWMGenPeriodSet(PWM1_BASE, PWM_GEN_3, Period);
 
-    //Set PWM duty-50% (Period /2)
-    PWMPulseWidthSet(PWM1_BASE, PWM_OUT_5,ulPeriod/2);
-    PWMPulseWidthSet(PWM1_BASE, PWM_OUT_6,ulPeriod/2);
-    PWMPulseWidthSet(PWM1_BASE, PWM_OUT_7,ulPeriod/2);
+    //Set PWM duty-50% 
+    PWMPulseWidthSet(PWM1_BASE, PWM_OUT_5,Period/2);
+    PWMPulseWidthSet(PWM1_BASE, PWM_OUT_6,Period/2);
+    PWMPulseWidthSet(PWM1_BASE, PWM_OUT_7,Period/2);
 
-    // Enable the PWM generator
+    // Enable the PWM Gen
     PWMGenEnable(PWM1_BASE, PWM_GEN_2);
     PWMGenEnable(PWM1_BASE, PWM_GEN_3);
 
     // Turn on the Output pins
     PWMOutputState(PWM1_BASE, PWM_OUT_5_BIT |PWM_OUT_6_BIT|PWM_OUT_7_BIT, true);
 
-    //Do nothing
    
  while (1)
  {
